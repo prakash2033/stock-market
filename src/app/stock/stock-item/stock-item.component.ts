7a27e1b6dd99a4ba8691d4715a3a1dc0d3bdfb87
Prakash Vishwakarma <pvishwa1@volvocars.com>
2023-04-18 11:35:14 +0200

    not stict initialization and stock-item update

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
-  styleUrls: ['./stock-item.component.css']
+  styleUrls: ['./stock-item.component.css'],
 })
-export class StockItemComponent {
+export class StockItemComponent implements OnInit {
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.name = 'Test stock company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+  }
 }
