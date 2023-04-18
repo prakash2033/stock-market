75f075f20f76861cca329eff508b650d428faf6a
Prakash Vishwakarma <pvishwa1@volvocars.com>
2023-04-18 11:08:58 +0200

    Initial stock market with stock-item

---
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css']
+})
+export class StockItemComponent {
+
+}
